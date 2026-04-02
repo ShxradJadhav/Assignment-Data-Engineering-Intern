44367535a4e5fdbe1b37115cb84f11d2a1030c84
Sharad Jadhav <sharadjadhav1999@gmail.com>
2026-04-02 17:11:27 +0530

    Add FastAPI endpoint to fetch jobs from database

--- api/main.py
+++ api/main.py
@@ -1,1 +1,25 @@
+from fastapi import FastAPI
+import mysql.connector
 
+app = FastAPI()
+
+@app.get("/jobs")
+def get_jobs():
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        database="CleanDB",
+        user="root",
+        password="root"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM final_jfp LIMIT 50")
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return rows
